4dc6beb6c959302cffd3fc5f0e34a20e69b929cf
Shivam Sharma <33760041+shivamsharma37372@users.noreply.github.com>
2021-08-05 15:30:07 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,7 @@
+let a= 200;
+let b =30;
+console.log("addition of two no is = ",a+b);
+console.log("addition of two no is = ",a-b);
+console.log("addition of two no is = ",a*b);
+console.log("addition of two no is = ",a%b);
+console.log("addition of two no is = ",a/b);
